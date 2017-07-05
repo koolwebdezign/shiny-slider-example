2161af4543d0ef900fc702c9a4c091aef2b1acaf
koolwebdezign <koolwebdezign@gmail.com>
2017-07-05 12:51:27 -0400

    Updated Shiny application

--- server.R
+++ server.R
@@ -1,14 +1,17 @@
 library(UsingR)
-data(galton)
 shinyServer(
-  function(input, output) {
-    output$newHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-      mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
-    })
-  }
+    function(input, output) {
+        output$newHist <- renderPlot({
+            in_n <- input$n
+            in_mu <- input$mu
+            in_sd <- input$sd
+            my_dist <- rnorm(in_n, mean = in_mu, sd = in_sd)
+            hist(my_dist, xlab = 'Normal Distribution', col = 'lightblue', main = 'Histogram')
+            abline(v=in_mu, col="red")
+            abline(v=in_mu-in_sd, col="blue")
+            abline(v=in_mu+in_sd, col="blue")
+            legend("topright", c("mean", "sd"), lwd=2, col=c("red", "blue"))
+        })
+        output$notes <- renderText({ "This is a simple demonstration of the random normal distribution. Please use the sliders in order to control the generation of a histogram." })
+    }
 )
--- ui.R
+++ ui.R
@@ -1,9 +1,23 @@
 shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
-  sidebarPanel(
-    sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05)
-  ),
-  mainPanel(
-    plotOutput('newHist')
-  )
+    headerPanel("Random Normal Distribution"),
+    sidebarPanel(
+        h2('Select (n)'),
+        helpText("A larger n will provide a more gaussian look to the histogram."),
+        sliderInput('n', 'Qty of Numbers', value = 5000, min = 1000, max = 10000, step = 500),
+        h2('Select (mu)'),
+        helpText("A larger mu will simply shift the horizontal axis."),
+        sliderInput('mu', 'Select a mean', value = 0, min = 0, max = 100, step = 1),
+        h2('Select (sd)'),
+        helpText("A larger sd will spread the distribution more widely on the horizontal axis."),
+        sliderInput('sd', 'Standard Deviation', value = 1, min = 1, max = 20, step = 1)
+    ),
+    mainPanel(
+        tabsetPanel(
+            tabPanel("Plot", plotOutput("newHist")),
+            tabPanel("Instructions", verbatimTextOutput("notes")),
+            tabPanel("Documentation",
+                 HTML("<pre>For complete documentation related to how this new Shiny app was produced with R Studio, please visit my <a href='https://github.com/koolwebdezign/shiny-slider-example' target='new'>GitHub Documentation</a>.</pre>")
+            )
+        )
+    )
 ))
